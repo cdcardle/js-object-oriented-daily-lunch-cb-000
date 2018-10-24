990216c2167a2c8bd15fe552b8fbf7266a658484
Chris Cardle <cdcardle@gmail.com>
2018-10-24 13:42:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ class Neighborhood {
     this.name = name;
   }
 }
+
+let mealId = 0;
+class Meal {
+  constructor(title, price) {
+    this.id = ++mealId;
+    this.title = title;
+    this.price = price;
+  }
+}
+
+let customerId = 0;
+class Customer {
+  constructor(name, neighborhoodId) {
+    this.id = ++customerId;
+    this.name = name;
+    neighborhoodId ? this.neighborhoodId = neighborhoodId; : nil;
+  }
+}
